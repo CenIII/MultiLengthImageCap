a0d86b540478988f20c6d5565e355d3a8506dfd1
yualiang <yualiang@umich.edu>
2019-03-27 20:45:17 -0400

    finish similar loss

--- crit/SimilarityLoss.py
+++ crit/SimilarityLoss.py
@@ -1,4 +1,5 @@
 import torch
+import torch.nn as nn
 import torch.nn.functional as F
 
 
@@ -13,14 +14,18 @@ def similarity_loss(text_images, size_info, gamma1, gamma2, gamma3): # consider 
     loss_reg = 0
 
     M, H_r, H_w = size_info
-    for e, v in text_images:
-        numerator, beta = torch.exp(calculate_matching_score(v, e, M, H_r, H_w, gamma1, gamma2))
+    for text_image in text_images:
+        e, v = text_image
+        numerator, beta = calculate_matching_score(v, e, M, H_r, H_w, gamma1, gamma2)
+        numerator = gamma3 * torch.exp(numerator)
         loss_reg += torch.norm(beta.mm(beta.t()) - torch.diag(beta.mm(beta.t())))
         P_DQ_denum = 0
         P_QD_denum = 0
         for e_sub, v_sub in text_images:
-            P_DQ_denum += torch.exp(calculate_matching_score(v, e_sub, M, H_r, H_w, gamma1, gamma2))
-            P_QD_denum += torch.exp(calculate_matching_score(v_sub, e, M, H_r, H_w, gamma1, gamma2))
+            denum, _ = calculate_matching_score(v, e_sub, M, H_r, H_w, gamma1, gamma2)
+            denum2, _ = calculate_matching_score(v_sub, e, M, H_r, H_w, gamma1, gamma2)
+            P_DQ_denum += gamma3 * torch.exp(denum)
+            P_QD_denum += gamma3 * torch.exp(denum2)
         loss1_w -= numerator / P_DQ_denum
         loss2_w -= numerator / P_QD_denum
     return loss1_w + loss2_w + loss_reg
@@ -37,11 +42,13 @@ def calculate_matching_score(v, e, M, H_r, H_w, gamma1, gamma2):
     :return: maching score for (Q, D) pair
     """
     T, _ = e.size()
-    similarity_matrix = e.mm(v.to())
+    similarity_matrix = e.mm(v.t())
+    # return torch.sum(similarity_matrix)
+
     # might consider overflow
     normalized_similarity_matrix = F.softmax(similarity_matrix, dim=0)
 
-    #regard text as query, might consider overflow
+    # #regard text as query, might consider overflow
     attn_score = F.softmax(gamma1 * normalized_similarity_matrix, dim=1)
     # each row of v_tidal represent the attention output
     v_tidal = attn_score.mm(v)
@@ -50,21 +57,29 @@ def calculate_matching_score(v, e, M, H_r, H_w, gamma1, gamma2):
         R_QD += v_tidal[i].view(1, -1).mm(e[i].view(-1, 1)).squeeze() / (torch.norm(v_tidal[i], 2) * torch.norm(e[i], 2))
     R_QD = torch.log(torch.pow(R_QD, 1 / gamma2))
 
+
     # regard image box as query, might consider overflow
     similarity_matrix_copy = normalized_similarity_matrix.clone()
     similarity_matrix_copy = torch.exp(similarity_matrix_copy)
-    beta = torch.zeros(T, M)
+
+    # beta = torch.zeros(T, M)
+    beta = []
     for i in range(M):
         local_sum = torch.sum(similarity_matrix_copy[:, i * H_r * H_w : (i + 1) * H_r * H_w])
-        similarity_matrix_copy[:, i * H_r * H_w : (i + 1) * H_r * H_w] /= local_sum
-        beta[:, i] = torch.sum(similarity_matrix_copy[:, i * H_r * H_w : (i + 1) * H_r * H_w], dim=1)
+        beta.append(torch.sum(torch.div(similarity_matrix_copy[:, i * H_r * H_w : (i + 1) * H_r * H_w], local_sum), dim=1))
+    # beta = torch.tensor(beta)
+    beta = torch.stack(beta, dim=0)
+    # return torch.sum(beta)
+
+    beta = beta.t()
     e_prime = (e.t()).mm(beta)
+
     # calculate R_QD in the second direction
     R_QD2 = 0
     for i in range(M):
         reference = 0
         for j in range(i * (H_r * H_w), (i + 1) * (H_r * H_w)):
-            reference += v[j].view(-1, 1).mm(e_prime[i].view(-1, 1)).squeeze() / (torch.norm(v[i], 2) * torch.norm(e_prime[i], 2))
+            reference += v[j].view(1, -1).mm(e_prime[:, i].view(-1, 1)).squeeze() / (torch.norm(v[j], 2) * torch.norm(e_prime[:, i], 2))
         reference /= H_r * H_w
         R_QD2 += torch.exp(reference)
     R_QD2 = torch.log(torch.pow(R_QD2, 1 / gamma2))
@@ -72,6 +87,18 @@ def calculate_matching_score(v, e, M, H_r, H_w, gamma1, gamma2):
     # add matching score for two directions.
     return R_QD + R_QD2, beta
 
+
+# class version of similarity loss
+class SimilarityLoss(nn.Module):
+
+    def __init__(self, gamma1, gamma2, gamma3):
+        super(SimilarityLoss, self).__init__()
+        self.gamma1 = gamma1
+        self.gamma2 = gamma2
+        self.gamma3 = gamma3
+
+    def forward(self, text_images, size_info):
+        return similarity_loss(text_images, size_info, gamma1, gamma2, gamma3)
 
 
 
@@ -87,14 +114,24 @@ if __name__ == "__main__":
     H_r = 10
     T = 5
     D = 20
-    e = torch.randn(T, D)
-    v = torch.randn(M, H_r, H_w, D)
-    v = v.view(M * H_r * H_w, D)
+    e = torch.rand(T, D, requires_grad=True)
+    v = torch.rand(M , H_r , H_w, D, requires_grad=True)
+    v1 = v.view(M * H_r * H_w, D)
     size_info = (M, H_r, H_w)
     gamma1 = 1
     gamma2 = 2
     gamma3 = 3
-    loss = similarity_loss((e, v), size_info, gamma1, gamma2, gamma3)
+    loss = similarity_loss([(e, v1)], size_info, gamma1, gamma2, gamma3)
+    loss.backward()
+    print(e.grad)
+    print(v1.grad)
+    print(v.grad)
+
+    print(loss)
+    # x = torch.randn(3, 4, requires_grad=True)
+    # y = torch.randn(3, 4, requires_grad=True)
+    # loss = test_loss(x, y)
+    # loss.backward()
 
 
 
