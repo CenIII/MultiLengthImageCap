ee42c406bd0cdadce221cb2ade10722949f3b095
CenIII <cenchuan2013@gmail.com>
2019-04-04 23:28:13 -0400

    ...

--- model/LinearModel.py
+++ model/LinearModel.py
@@ -6,15 +6,15 @@ class LinearModel(nn.Module):
     def __init__(self, hiddenSize=1024):
         super(LinearModel, self).__init__()
         self.conv1 = nn.Conv2d(512, hiddenSize, (10, 9), (4, 6))
-        self.bn1 = nn.BatchNorm2d(hiddenSize)
+        # self.bn1 = nn.BatchNorm2d(hiddenSize)
         self.conv2 = nn.Conv2d(512, hiddenSize, 1, 1)
-        self.bn2 = nn.BatchNorm2d(hiddenSize)
+        # self.bn2 = nn.BatchNorm2d(hiddenSize)
 
     def forward(self, box_feat, global_feat):
         global_feat = global_feat.unsqueeze(dim=0)
-        global_feat = self.bn1(self.conv1(global_feat))
+        global_feat = self.conv1(global_feat)
         global_hidden = F.avg_pool2d(global_feat, 7).squeeze()
-        box_feat = self.bn2(self.conv2(box_feat))
+        box_feat = self.conv2(box_feat)
         return global_feat, global_hidden, box_feat
 
 
