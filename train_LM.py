cd3df33b8ff1abee64bb11fd912a230fb8d91834
ruoyaow <ruoyaow@umich.edu>
2019-04-04 21:27:54 -0400

    adjust loss compution in lm training

--- train_LM.py
+++ train_LM.py
@@ -19,6 +19,9 @@ def train_LM(lmloader, model, optimizer, criterion, pad_id, max_epoch):
             batch_size = input_sentences.shape[0]
             decoder_output, _, _ = model(input_sentences, teacher_forcing_ratio=1)
             decoder_output_reshaped = torch.cat([decoder_output[i].unsqueeze(1) for i in range(len(decoder_output))],1)
+            vocab_size = decoder_output_reshaped.shape[2]
+            decoder_output_reshaped = decoder_output_reshaped.view(-1, vocab_size)
+            input_sentences = input_sentences[:,1:].view(-1)
             loss = criterion(decoder_output_reshaped, input_sentences[:,1:])
             loss.backward()
             optimizer.step()
