4941824f9db2d01efee597c3c162e2bb77a31694
CenIII <cenchuan2013@gmail.com>
2019-04-11 21:30:11 -0400

    ...

--- dataloader/dataloader.py
+++ dataloader/dataloader.py
@@ -133,7 +133,8 @@ class LoaderEnc(_BaseDataLoader):
 			lengths = torch.zeros(batchSize,dtype=torch.int32)
 			for i in range(batchSize):
 				cap = caps[i]
-				lengths[i] = (cap==0).nonzero()[0][0]
+				nonz = (cap==0).nonzero()
+				lengths[i] = nonz[0][0] if len(nonz)>0 else len(cap)
 			return lengths
 		for i in range(numImgs):
 			data = batch[i][0]
