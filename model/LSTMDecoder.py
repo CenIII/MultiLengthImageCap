3c39570c7384b4d51e0b2bf6b9e58f70e744b50e
CenIII <cenchuan2013@gmail.com>
2019-04-18 18:51:04 -0400

    ...

--- model/LSTMDecoder.py
+++ model/LSTMDecoder.py
@@ -118,7 +118,7 @@ class DecoderRNN(BaseRNN):
         return predicted_softmax, hidden, attn
 
     def forward(self, inputs=None, encoder_hidden=None, encoder_outputs=None,
-                    function=F.log_softmax, teacher_forcing_ratio=0):
+                    function=F.softmax, teacher_forcing_ratio=0):
         ret_dict = dict()
         if self.use_attention:
             ret_dict[DecoderRNN.KEY_ATTN_SCORE] = list()
