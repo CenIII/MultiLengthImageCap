5fcd6beea50eeb56f1416bb4384a5eb1c410cfb8
CenIII <cenchuan2013@gmail.com>
2019-04-21 00:15:03 -0400

    ...

--- model/LSTMDecoder.py
+++ model/LSTMDecoder.py
@@ -138,7 +138,7 @@ class DecoderRNN(BaseRNN):
             decoder_outputs.append(step_output)
             if self.use_attention:
                 ret_dict[DecoderRNN.KEY_ATTN_SCORE].append(step_attn)
-            symbols = decoder_outputs[-1].topk(2)[1][0][1]
+            symbols = [decoder_outputs[-1].topk(2)[1][0][1]]
             sequence_symbols.append(symbols)
 
             eos_batches = symbols.data.eq(self.eos_id)
