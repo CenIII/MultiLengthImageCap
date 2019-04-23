1de42d30792f6b9105ecbf0c610cead9f7f3abd2
CenIII <cenchuan2013@gmail.com>
2019-04-22 21:32:17 -0400

    ...

--- crit/SimilarityLoss.py
+++ crit/SimilarityLoss.py
@@ -340,6 +340,8 @@ class SimilarityLoss(nn.Module):
         loss1 = 0
         loss_reg = 0
         for i in range(0,B,self.bsize):
+            if i+self.bsize>B:
+                i -= ((i+self.bsize)-B)
             image_b = image[i:i+self.bsize]
             text_b = torch.cat([text[j][:length_info[j]] for j in range(i,min(i+self.bsize,B))],dim=0).contiguous()
             len_b = [torch.sum(length_info[i:j+1]) for j in range(i,min(i+self.bsize,B))]
