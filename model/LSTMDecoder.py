d3a1b48cc251c20c462321663a423909cc6d14f6
CenIII <cenchuan2013@gmail.com>
2019-04-24 18:20:59 -0400

    ...

--- model/LSTMDecoder.py
+++ model/LSTMDecoder.py
@@ -164,11 +164,12 @@ class DecoderRNN(BaseRNN):
                 lengths[update_idx] = len(sequence_symbols)
             return symbols
         def getTopkIndsnScores(candScores):
-            tmp = candScores.squeeze()
-            B,V = tmp.shape
-            zzz = torch.topk(tmp.view(-1),self.topk)
-            bmScores_nxt = zzz[0]
-            bmTopkInds_nxt = torch.stack([zzz[1]/V,zzz[1]%V],dim=1)
+            tmp = candScores[:,:,0].permute(1,0,2).contiguous()
+            B,K,V = tmp.shape
+            zzz = torch.topk(tmp.view(B,-1),self.topk,dim=1)
+            bmScores_nxt = zzz[0].transpose(0,1) # B,K
+            inds = zzz[1].transpose(0,1)
+            bmTopkInds_nxt = torch.stack([inds/V,inds%V],dim=2)
             return bmTopkInds_nxt, bmScores_nxt
 
 
