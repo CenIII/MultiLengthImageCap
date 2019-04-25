0cd54fda1e838947360fb7b4991ee2773aa0ca45
CenIII <cenchuan2013@gmail.com>
2019-04-24 21:42:36 -0400

    ...

--- model/LinearModel.py
+++ model/LinearModel.py
@@ -3,29 +3,24 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 class LinearModel(nn.Module):
-    def __init__(self, hiddenSize=1024):
+    def __init__(self, encHiddenSize=4096, decHiddenSize=1024):
         super(LinearModel, self).__init__()
-        self.conv1 = nn.Conv2d(512, hiddenSize, 7, stride=2)
-        self.conv2 = nn.Conv2d(512, hiddenSize, 5 ,stride=1)
-        self.hiddenSize = hiddenSize
+        self.conv1 = nn.Conv2d(encHiddenSize, decHiddenSize, 1, stride=1)
+        self.conv2 = nn.Conv2d(512, encHiddenSize, 5 ,stride=1)
+        self.encHiddenSize = encHiddenSize
+        self.decHiddenSize = decHiddenSize
 
     def forward(self, box_feat, global_feat): # box_feat [8, 2, 512, 7, 7], globel_feat list of [512, 26, 45]
         B,M = box_feat.size()[:2]
         box_feat = box_feat.view(-1,512,7,7)
-        box_feat = self.conv2(box_feat).view(B,M,self.hiddenSize,3,3)
+        box_feat = self.conv2(box_feat)
 
-        global_vec = []
-        for i in range(len(global_feat)):
-            global_vec.append(self.conv1(global_feat[i].unsqueeze(0)).view(self.hiddenSize,-1).max(dim=1)[0])
-        global_vec = torch.stack(global_vec,dim=0)
-        return box_feat, global_vec # box_feat [8,2,hiddensize,3,3], global_vec [8, hidden_size]
+        # global_vec = []
+        # for i in range(len(global_feat)):
+        #     global_vec.append(self.conv1(global_feat[i].unsqueeze(0)).view(self.hiddenSize,-1).max(dim=1)[0])
+        # global_vec = torch.stack(global_vec,dim=0)
 
-
-# if __name__ == "__main__":
-#     global_feat = torch.randn(512, 34, 45)
-#     box_feat = torch.randn(75, 512, 7, 7)
-#     image_pair = [global_feat, box_feat]
-
-#     lm = LinearModel()
-#     gf, gh, bf = lm(image_pair)
-#     print(gf.shape, gh.shape, bf.shape)
+        box_feat_dec = self.conv1(box_feat).view(B,M,self.decHiddenSize,3,3)
+        box_feat = box_feat.view(B,M,self.encHiddenSize,3,3)
+        global_vec = box_feat_dec.permute(0,2,1,3,4).contiguous().view(B,self.decHiddenSize,-1).max(dim=2)[0]
+        return box_feat, box_feat_dec, global_vec # box_feat [8,2,hiddensize,3,3], global_vec [8, hidden_size]
